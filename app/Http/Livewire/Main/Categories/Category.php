198009888e1d69aa033ea393736f9fa7a0a06649
Ennnvvvy <deposoyivanrey_bsit@plmun.edu.ph>
2023-04-11 17:12:48 +0800

    Done: product display, filters, Todo: product search, add to cart, checkout, products db modify

--- app/Http/Livewire/Main/Categories/Category.php
+++ app/Http/Livewire/Main/Categories/Category.php
@@ -13,47 +13,49 @@ class Category extends Component
     use WithPagination;
     public $category;
     
-
     public $min_price = 0;
     public $max_price = 0;
+    public $brands = [];
 
-   protected $queryString = ['min_price', 'max_price'];
-   protected $paginationTheme = 'bootstrap';
-    
-   public function updated($property)
+    protected $listeners = ['filtersReset' => '$refresh'];
+
+
+    protected $queryString = ['min_price', 'max_price', 'brands'];
+    protected $paginationTheme = 'bootstrap';
+
+    public function updated($property)
     {
-        if ($property == 'min_price' or $property =='max_price')
-        {
+        if ($property == 'min_price' or $property == 'max_price' or $property == 'brands') {
+            $this->emitSelf('filtersReset');
             $this->resetPage();
         }
     }
-  
+
     public function mount($id)
     {
         $this->category = ModelsCategory::find($id);
     }
-    
+
     public function render()
     {
-        $product_list = Products::where('category_id', $this->category->id)
-        ->where('status', 1)
-        ->has('stocks');
-        
-        if ($this->min_price) {
-            $product_list->whereHas('stocks', function ($query) {
-                $query->where('selling_price', '>=', $this->min_price);
-            });
-        }
-        if ($this->max_price) {
-            $product_list->whereHas('stocks', function ($query) {
-                $query->where('selling_price', '<=', $this->max_price);
-            });
-        }
+        $product_list = Products::with('stocks')
+            ->where('category_id', $this->category->id)
+            ->when($this->brands, function ($query, $brands) {
+                $query->whereIn('brand_id', $brands);
+            })
+            ->when($this->min_price, function ($query, $min_price) {
+                $query->whereHas('stocks', function ($query) use ($min_price) {
+                    $query->where('selling_price', '>=', $min_price);
+                });
+            })
+            ->when($this->max_price, function ($query, $max_price) {
+                $query->whereHas('stocks', function ($query) use ($max_price) {
+                    $query->where('selling_price', '<=', $max_price);
+                });
+            })->where('status', 1)->has('stocks');
 
-       
-        
-        return view('livewire.main.categories.category',[
-            'product_list' => $product_list->latest()->get(),
+        return view('livewire.main.categories.category', [
+            'product_list' => $product_list->latest()->get()
         ])->extends('layouts.main');
     }
 }
--- resources/views/livewire/main/categories/category.blade.php
+++ resources/views/livewire/main/categories/category.blade.php
@@ -1,114 +1,107 @@
 <section class="content" class="d-flex">
     <div class="sidebar bg-light border shadow-sm" id="side_nav">
-       <div class="px-2 pt-5">
-          <div class="card bg-body border-0">
-             <div class="card-body">
-                <div class="card-title">
-                   <h6 class="fw-bold">Brands</h6>
+        <div class="px-2 pt-5 pt-md-1">
+            <h1 class="px-2 fw-bold fs-3">
+                Filters
+            </h1>
+        </div>
+
+        <div class="px-2">
+            <div class="card bg-body border-0">
+                <div class="card-header border-0 text-bg-dark rounded">
+                    <h6 class="card-title fw-bold m-0">Brands</h6>
                 </div>
-
-             </div>
-          </div>
-       </div>
-
-       <hr class="h-color mx-2">
-
-       <div class="px-2">
-          <div class="card bg-body border-0">
-             <div class="card-body">
-                <div class="card-title">
-                   <h6 class="fw-bold">Sort by price</h6>
+                <div class="card-body">
+                <div class="card-text">
+                    @foreach($category->brands as $brand)
+                        <div class="form-check pb-1">
+                            <input class="form-check-input" type="checkbox" value="{{ $brand->id }}" id="brand_in_category" 
+                            wire:model.debounce.500ms="brands">
+                            <label class="form-check-label fw-bold" for="brand_in_category">
+                            {{ $brand->name }}
+                            </label>
+                        </div>
+                    @endforeach
                 </div>
-                
+                </div>
+            </div>
+        </div>
+
+        <div class="px-2">
+            <div class="card bg-body border-0">
+                <div class="card-header border-0 text-bg-dark rounded">
+                <h6 class="card-title fw-bold m-0">Prices</h6>
+                </div>
+                <div class="card-body">
                 <div class="d-flex justify-content-between">
-                   <div class="">
-                      <input type="number" wire:model="min_price" class="form-control">
-                      {{-- <label for="name" class="form-label fw-bold">Brand </label>
-                      <input type="text" class="form-control @error('name') is-invalid  @enderror
-                      @if($name) '' @endif"
-                      id="name" placeholder="Brand Name"
-                      wire:model.debounce.500ms="name">
-                      @error('name') 
-                      <small class="text-danger">{{ $message }}</small>
-                      @enderror --}}
-                   </div>
-
-                   <div class="px-2 pt-1">
-                      <span>
-                         <i class="fa-solid fa-minus"></i>
-                      </span>
-                   </div>
-
-                   <div class="">
-                      <input type="number"  wire:model="max_price" class="form-control">
-                   </div>
+                    <div class="">
+                        <label for="min_price" class="form-label fw-bold mb-0">Min</label>
+                        <input type="number" id="min_price" wire:model.debounce.500ms="min_price" class="form-control">
+                    </div>
+                    <div class="px-2 pt-4">
+                        <span>
+                        <i class="fa-solid fa-minus"></i>
+                        </span>
+                    </div>
+                    <div class="">
+                        <label for="max_price" class="form-label fw-bold mb-0">Max</label>
+                        <input type="number" id="max_price" wire:model.debounce.500ms="max_price" class="form-control">
+                    </div>
                 </div>
-
-             </div>
-          </div>
-       </div>
+                </div>
+            </div>
+        </div>
     </div>
 
     <div class="d-md-none d-block" id="opn-cls-btn">
        <button class="btn px-1 py-0 open-btn">
-          <i class="fa-solid fa-bars-staggered"></i>
+       <i class="fa-solid fa-bars-staggered"></i>
        </button>
-    </div> 
+    </div>
 
     <div class="px-4 py-5">
-        <div>
-            <h1>
-                <a href="{{ route('main.categories') }}" class="link-dark breadcrumbs">Categories</a> /
-                <a href="" class="breadcrumbs link-secondary">{{ $category->name }}</a>
-            </h1>
-        
-            <div class="row px-3 pt-3">
-               
-                @if(empty($product_list))
-                    <div class="col">
-                        <div class="alert alert-danger">
-                            No products found...
+       <div>
+          <h1>
+             <a href="{{ route('main.categories') }}" class="link-dark breadcrumbs">Categories</a> /
+             <a href="" class="breadcrumbs link-secondary">{{ $category->name }}</a>
+          </h1>
+          <div class="row px-3 pt-3" >
+            @if($product_list->count() == 0)
+                <div class="col-12">
+                    <div class="alert alert-danger">
+                        <span class="fw-bold">No products found...</span>
+                    </div>
+                </div>
+            @endif
+
+            @forelse($product_list as $product)
+            <div class="col-6 col-md-4 col-lg-2 p-1">
+                <a id="product" href="{{ route('main.product', ['slug' => $product->slug, 'id' => $product->id ])}}" class="text-decoration-none">
+                    <div class="card bg-body shadow-sm">
+                        <img src="{{ asset('storage/'.$product->images->first()->filename ) }}" class="card-img-top" alt="{{ $product->name }} image" height="160px">
+                        <div class="card-body p-2 m-0">
+                            <div class="card-text p-0 m-0 mb-1">
+                            <h6 class="fw-bold link-dark pt-1">
+                                {{  $product->name }}
+                            </h6>
+                            </div>
+                            <h6 class="text-warning m-0 p-0">
+                            {{-- <span class="text-dark fs-5">P {{ $product->stocks[0]->selling_price }} --}}
+                            <span class="text-dark fs-5">P {{ $product->stocks->first()->selling_price }}
+                            </span>
+                            </h6>
+                            <div class="d-flex justify-content-between">
+                            <small class="text-danger">
+                            <span class="text-dark">P</span>
+                            <s>{{ $product->stocks->first()->original_price }}</s>
+                            </small>
+                            </div>
                         </div>
                     </div>
-                @endif
-               
-                @foreach($product_list as $product)
-               
-                    <div class="col-6 col-md-4 col-lg-2 p-1">
-                        <a id="product" href="{{ route('main.product', ['slug' => $product->slug, 'id' => $product->id ])}}" class="text-decoration-none">
-                            <div class="card bg-body shadow-sm">
-                                <img src="{{ asset('storage/'.$product->images->first()->filename ) }}" class="card-img-top" alt="{{ $product->name }} image" height="160px">
-                                <div class="card-body p-2 m-0">
-                                    <div class="card-text p-0 m-0 mb-1">
-                                        <h6 class="fw-bold link-dark pt-1">
-                                            {{  $product->name }}
-                                        </h6>
-                                    </div>
-                                    
-                                    <h6 class="text-warning m-0 p-0">
-                                     
-                                        {{-- <span class="text-dark fs-5">P {{ $product->stocks[0]->selling_price }} --}}
-                                        
-                                        <span class="text-dark fs-5">P {{ $product->stocks->first()->selling_price }}
-                                
-                                        </span>
-                                    </h6>
-                                    
-                                    <div class="d-flex justify-content-between">
-                                        <small class="text-danger">
-                                            <span class="text-dark">P</span>
-                                            <s>{{ $product->stocks->first()->original_price }}</s>
-                                        </small>
-                                    </div>  
-                                </div>
-                            </div>
-                        </a>
-                    </div>
-                @endforeach
+                </a>
             </div>
-        </div>
+            @endforeach
+          </div>
+       </div>
     </div>
  </section>
-
-
-
