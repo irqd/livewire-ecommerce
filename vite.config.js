98b79ce7ed6066874da341dced9675f22671aede
Alfred Umali Paguio <alfredpaguio36@gmail.com>
2023-04-20 12:12:27 +0800

    Update vite.config.js
    Used
    [https://rollupjs.org/configuration-options/#output-manualchunks] to improve chunking

--- vite.config.js
+++ vite.config.js
@@ -6,17 +6,33 @@ import path from 'path'
 export default defineConfig({
     plugins: [
         laravel({
-            input: [
-                'resources/js/app.js',
-            ],
+            input: ['resources/js/app.js'],
             refresh: true,
         }),
     ],
-    
+
     resolve: {
         alias: {
             '~bootstrap': path.resolve(__dirname, 'node_modules/bootstrap'),
             '~fa': path.resolve(__dirname, 'node_modules/@fortawesome/fontawesome-free/scss'),
         }
     },
+
+    build: {
+        chunkSizeWarningLimit: 1500,
+        rollupOptions: {
+            output: {
+                manualChunks(id) {
+                    if (id.includes('node_modules/bootstrap')) {
+                        return 'bootstrap';
+                    }
+                    if (id.includes('node_modules/@fortawesome/fontawesome-free')) {
+                        return 'fontawesome';
+                    }
+                },
+            },
+        },
+    },
+
+
 });
